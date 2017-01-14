f8c246bbcf18f607a9bec32810c42fbcc4a1f199
Neson <neson@dex.tw>
2017-01-15 02:04:35 +0800

    remove "test" folder

--- test/dummy/config/initializers/auth_provider.rb
+++
@@ -1,7 +0,0 @@
-AuthProvider.configure do
-  default_resource_owner_type "User"
-
-  resource_owner_from_credentials do |_type, username, password|
-    User.find_by(username: username, password: password)
-  end
-end
