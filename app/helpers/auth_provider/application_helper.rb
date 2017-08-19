2c2d2da1969293b5e205a1eee2b579b7ad0fa6d9
zetavg <mail@zeta.vg>
2017-08-20 01:30:04 +0800

    add authenticated_resource_owner helper

--- app/helpers/auth_provider/application_helper.rb
+++ app/helpers/auth_provider/application_helper.rb
@@ -1,4 +1,10 @@
 module AuthProvider
   module ApplicationHelper
+    def authenticated_resource_owner
+      access_token = request.headers[:Authorization]
+      return unless access_token
+      access_token = access_token.split(' ').last
+      AuthProvider.resource_owner_from_token(access_token)
+    end
   end
 end
--- lib/auth_provider/engine.rb
+++ lib/auth_provider/engine.rb
@@ -1,5 +1,10 @@
 module AuthProvider
   class Engine < ::Rails::Engine
     isolate_namespace AuthProvider
+
+    config.to_prepare do
+      ::ApplicationController.include(AuthProvider::Engine.helpers)
+      ::ApplicationController.helper(AuthProvider::Engine.helpers)
+    end
   end
 end
